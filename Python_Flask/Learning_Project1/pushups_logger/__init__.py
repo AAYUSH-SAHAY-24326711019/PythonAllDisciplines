8e62b11459f3bfbbdf7ade5b3d39a285cb51ab32
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2026-02-16 18:52:45 +0530

    working signup process to register

--- Python_Flask/Learning_Project1/pushups_logger/__init__.py
+++ Python_Flask/Learning_Project1/pushups_logger/__init__.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+db = SQLAlchemy()
+
+
 
 def create_app():
     app = Flask(__name__)
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    #code for the sql alchemy
+    app.config['SECRET_KEY']='secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+
+    db.init_app(app)
+    from .models import User   
+
+    with app.app_context():
+        db.create_all()      
+
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
